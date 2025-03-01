eeec3257f3be9963080a94816b6f7a44eb8f6c49
“Deepak” <deepak.ghadi@idsnext.com>
2025-03-01 16:43:01 +0530

    First commit

--- Sea Food CoreML/ViewController.swift
+++ Sea Food CoreML/ViewController.swift
@@ -6,14 +6,84 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage into CIImage")
+            }
+            detect(image: ciimage)
+        }
+        picker.dismiss(animated: true, completion: nil)
+        
+        
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3(configuration: MLModelConfiguration()).model) else {
+            fatalError("Loding Core ML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) {request , error  in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            print(results)
+            
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog"
+                }else {
+                    self.navigationItem.title = "Not Hotdog"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }catch {
+            print(error)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraBtnClicked(_ sender: UIBarButtonItem) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    present(imagePicker, animated: true, completion: nil)
+                } else {
+                    print("Camera not available")
+                    let alert = UIAlertController(title: "Error", message: "Camera not available on this device", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default))
+                    present(alert, animated: true)
+                }
+    }
+    
 
 }
 
